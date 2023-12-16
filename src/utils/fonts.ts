503f4209fb0616c93eed153bb9d492694b07e54f
sittichok Ouamsiri <sutichok46@gmail.com>
2023-12-16 19:08:17 +0700

    fix: migrate font

---
+++ src/utils/fonts.ts
@@ -0,0 +1,4 @@
+import {IBM_Plex_Sans_Thai} from "next/font/google";
+
+export const ibm = IBM_Plex_Sans_Thai({ subsets: ["thai"], weight: "400" });
+export const ibmBold = IBM_Plex_Sans_Thai({ subsets: ["thai"], weight: "600" });
