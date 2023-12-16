62bb89195c03d292fa80796c40b8a53186d8d6d8
sittichok Ouamsiri <sutichok46@gmail.com>
2023-12-16 19:08:34 +0700

    fix: ibm font to agenda page

--- src/components/main/agenda.tsx
+++ src/components/main/agenda.tsx
@@ -2,16 +2,14 @@ import Image from "next/image";
 import React from "react";
 import AgendaBg from "@/assets/agenda.svg";
 import AgendaLogo from "@/assets/agenda-logo.svg";
-import { Modak, Raleway } from "next/font/google";
 import { cls } from "@/utils/misc";
+import { ibmBold} from "@/utils/fonts";
 
-const modak = Modak({ subsets: [], weight: "400" });
-const raleway = Raleway({ subsets: [], weight: "600" });
 
 const Agenda = () => {
   return (
     <div
-      className={cls("flex flex-col items-center md:pt-0 pt-[70px] relative")}
+      className={cls(ibmBold.className, "flex flex-col items-center md:pt-0 pt-[70px] relative")}
     >
       <div className="absolute top-[6%] flex justify-center">
         <Image
@@ -21,7 +19,6 @@ const Agenda = () => {
         />
         <p
           className={cls(
-            modak.className,
             "xl:text-[6.5vw] md:text-[70px] md:px-32 text-[42px] px-56 text-center leading-none text-yellow-stroke-black-2px"
           )}
         >
@@ -30,7 +27,6 @@ const Agenda = () => {
       </div>
       <div
         className={cls(
-          raleway.className,
           "flex flex-col items-end absolute md:top-[30%] top-[37%] md:mr-[64%] mr-[69%]"
         )}
       >
@@ -41,18 +37,16 @@ const Agenda = () => {
       </div>
       <div
         className={cls(
-          raleway.className,
           "flex flex-col items-start absolute top-[41%] ml-[68%]"
         )}
       >
         <p className={cls("md:text-[2.5vw] text-[3vw] text-[#2A3D5D]")}>
           23 ธ.ค. 66
         </p>
-        <p className="text-[4vw] md:text-[2.7vw]">ประกาศผลรอบ 1</p>
+        <p className="text-[4vw] font-extrabold md:text-[2.7vw]">ประกาศผลรอบ 1</p>
       </div>
       <div
         className={cls(
-          raleway.className,
           "flex flex-col items-end absolute top-[54%] mr-[52%] md:top-[50%]"
         )}
       >
@@ -63,7 +57,6 @@ const Agenda = () => {
       </div>
       <div
         className={cls(
-          raleway.className,
           "flex flex-col items-start absolute top-[70%] ml-[45%] md:top-[68%] md:ml-[40%]"
         )}
       >
