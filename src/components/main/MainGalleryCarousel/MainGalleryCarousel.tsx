41655bba9627b77402c515e562dbefd9e3500a6e
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2023-12-21 17:56:39 +0700

    fix: carousel images

--- src/components/main/MainGalleryCarousel/MainGalleryCarousel.tsx
+++ src/components/main/MainGalleryCarousel/MainGalleryCarousel.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import Carousel0 from "@/assets/carousels/0.jpg";
 import Carousel1 from "@/assets/carousels/1.jpg";
+import Carousel2 from "@/assets/carousels/2.jpg";
+import Carousel3 from "@/assets/carousels/3.jpg";
+import Carousel4 from "@/assets/carousels/4.jpg";
 
 const MainCarouselCarousel = () => {
-  const images = [Carousel1, Carousel1, Carousel1, Carousel1, Carousel1];
+  const images = [Carousel0, Carousel1, Carousel2, Carousel3, Carousel4];
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState<string | null>(null);
 
@@ -31,12 +35,7 @@ const MainCarouselCarousel = () => {
       },
     },
   };
-  const slidersVariants = {
-    hover: {
-      scale: 1.2,
-      backgroundColor: "#ff00008e",
-    },
-  };
+
   const dotsVariants = {
     initial: {
       y: 0,
@@ -52,12 +51,12 @@ const MainCarouselCarousel = () => {
     },
   };
 
-  const handleNext = () => {
+  const handleNext = useCallback(() => {
     setDirection("right");
     setCurrentIndex((prevIndex) =>
       prevIndex + 1 === images.length ? 0 : prevIndex + 1
     );
-  };
+  }, [images.length]);
 
   const handlePrevious = () => {
     setDirection("left");
@@ -71,6 +70,15 @@ const MainCarouselCarousel = () => {
     setDirection(index > currentIndex ? "right" : "left");
     setCurrentIndex(index);
   };
+  const [isHover, setIsHover] = useState(false);
+  // auto slide
+  useEffect(() => {
+    if (isHover) return;
+    const interval = setInterval(() => {
+      handleNext();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [handleNext, isHover]);
 
   return (
     <div
@@ -79,7 +87,11 @@ const MainCarouselCarousel = () => {
         maxWidth: "1280px",
         paddingLeft: "2.5rem",
         paddingRight: "2.5rem",
+        position: "relative",
       }}
+      role="banner"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
       <div className="carousel">
         <div className="carousel-images">
@@ -93,39 +105,29 @@ const MainCarouselCarousel = () => {
               variants={slideVariants}
             />
           </AnimatePresence>
-          <div className="slide_direction">
-            <motion.div
-              variants={slidersVariants}
-              whileHover="hover"
-              className="left"
-              onClick={handlePrevious}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="20"
-                viewBox="0 96 960 960"
-                width="20"
-              >
-                <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-              </svg>
-            </motion.div>
-            <motion.div
-              variants={slidersVariants}
-              whileHover="hover"
-              className="right"
-              onClick={handleNext}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="20"
-                viewBox="0 96 960 960"
-                width="20"
-              >
-                <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-              </svg>
-            </motion.div>
-          </div>
         </div>
+      </div>
+      <div className="slide_direction">
+        <motion.div className="left" onClick={handlePrevious}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 96 960 960"
+            width="20"
+          >
+            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+          </svg>
+        </motion.div>
+        <motion.div className="right" onClick={handleNext}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 96 960 960"
+            width="20"
+          >
+            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+          </svg>
+        </motion.div>
       </div>
       <div className="carousel-indicator">
         {images.map((image, index) => (
