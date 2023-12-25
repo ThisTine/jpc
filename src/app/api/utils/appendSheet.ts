f787d83c097ea3dd41ef0ce873cb6045c4a7eb02
sittichok Ouamsiri <sutichok46@gmail.com>
2023-12-25 20:57:45 +0700

    Development (#22)
    * chore: add class utility.
    
    * chore: settings
    
    * feat: misc classname
    
    * add: title and info
    
    * add: cs sit info
    
    * update eslint
    
    * add: faq and contact us
    
    * chore: add class utility.
    
    * chore: settings
    
    * feat: misc classname
    
    * add: title and info
    
    * add: cs sit info
    
    * update eslint
    
    * add: faq and contact us
    
    * add: cursor pointer
    
    * add: tablet design
    
    * add: agenda
    
    * fix: horizontal overflow
    
    * fix: horizontal overflow
    
    * fix: migrate font
    
    * fix: ibm font to agenda page
    
    * feat: framer motion
    
    * feat: mobile overflow
    
    * feat: add content
    
    * add: react-hook-form
    
    * feat: add zod, resolver
    
    * feat: animation
    
    * feat: prepared form
    
    * feat: stepper
    
    * add: ig, facebook link
    
    * fix font and title
    
    * feat: react-imask
    
    * fix: localStorage
    
    * fix: alpine
    
    * fix: migrate formData
    
    * feat: formData API
    
    * add css for register
    
    * fix: missing font import
    
    * update faq question
    
    * feat: register mail
    
    * add: link to register
    
    * fix: mobile title
    
    * fix: stepper width and maxWidth container
    
    * feat: add register button animation
    
    * feat: add bounce animation on jigsaw
    
    * Update page.tsx
    
    * feat: animation wow!
    
    * Update faq-contact.tsx
    
    * Update register test question
    
    * fix faq question
    
    * feat: add animation
    
    * fix: fix pnpm
    
    * fix: fix pnpm
    
    * fix: unoptimized
    
    * fix: register form title
    
    * fix: mail
    
    * feat: add og image
    
    * feat: add web
    
    * fix: wrong padding jpc
    
    * feat: ping animation
    
    * Update formData.ts
    
    * feat: form validator
    
    * feat: form
    
    * fix:(registerPage): code image test
    
    * fix(multiplechoice): value
    
    * fix: wording and question
    
    * Update page.tsx
    
    * add : carousel container
    
    * fix: logic
    
    * fix: animation
    
    * fix: temporary disable carousel
    
    * fix: animation
    
    * fix: temporary disable carousel
    
    * fix: email validate and step persistent
    
    * fix: add logs
    
    * fix: discordbot log
    
    * fix: separate url
    
    * fix: text
    
    * fix: text space
    
    * feat: carousel
    
    * fix: text label
    
    * feat: error page
    
    * feat: input phone masked
    
    * fix: carousel images
    
    * fix: loading state
    
    * fix z-index
    
    * fix: form
    
    * fix: sendemail
    
    * fix: date
    
    * fix: clean up
    
    ---------
    
    Co-authored-by: Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
    Co-authored-by: Jirasin <jjrsinx@gmail.com>
    Co-authored-by: Jirasin Jarethammajit <69670650+jirasin02@users.noreply.github.com>

---
+++ src/app/api/utils/appendSheet.ts
@@ -0,0 +1,21 @@
+import {getGoogleAuth} from "@/app/api/utils/getGoogleAuth";
+import {google} from "googleapis";
+import dayjs from "dayjs";
+
+export const appendSheet = (sheetsName:"Basic infomation"|"Academic", values: string[])=>{
+  const auth = getGoogleAuth();
+  const service = google.sheets({version: 'v4', auth});
+  const spreadSheetId = process.env.SPREAD_SHEET_ID;
+  return service.spreadsheets.values.append({
+    spreadsheetId: spreadSheetId,
+    range: sheetsName,
+    valueInputOption: "USER_ENTERED",
+    requestBody:{
+      range: sheetsName,
+      values:[
+        [dayjs().format("MM/DD/YYYY HH:mm:ss"),
+          ...values]
+      ]
+    }
+  });
+};
