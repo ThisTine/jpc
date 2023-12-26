8d715293576b721766a460779a0d039f666f8f6f
sittichok Ouamsiri <sutichok46@gmail.com>
2023-12-26 22:55:05 +0700

    Development (#23)
    * chore: add class utility.
    
    * chore: settings
    
    * feat: misc classname
    
    * add: title and info
    
    * add: cs sit info
    
    * update eslint
    
    * add: faq and contact us
    
    * chore: add class utility.
    
    * chore: settings
    
    * feat: misc classname
    
    * add: title and info
    
    * add: cs sit info
    
    * update eslint
    
    * add: faq and contact us
    
    * add: cursor pointer
    
    * add: tablet design
    
    * add: agenda
    
    * fix: horizontal overflow
    
    * fix: horizontal overflow
    
    * fix: migrate font
    
    * fix: ibm font to agenda page
    
    * feat: framer motion
    
    * feat: mobile overflow
    
    * feat: add content
    
    * add: react-hook-form
    
    * feat: add zod, resolver
    
    * feat: animation
    
    * feat: prepared form
    
    * feat: stepper
    
    * add: ig, facebook link
    
    * fix font and title
    
    * feat: react-imask
    
    * fix: localStorage
    
    * fix: alpine
    
    * fix: migrate formData
    
    * feat: formData API
    
    * add css for register
    
    * fix: missing font import
    
    * update faq question
    
    * feat: register mail
    
    * add: link to register
    
    * fix: mobile title
    
    * fix: stepper width and maxWidth container
    
    * feat: add register button animation
    
    * feat: add bounce animation on jigsaw
    
    * Update page.tsx
    
    * feat: animation wow!
    
    * Update faq-contact.tsx
    
    * Update register test question
    
    * fix faq question
    
    * feat: add animation
    
    * fix: fix pnpm
    
    * fix: fix pnpm
    
    * fix: unoptimized
    
    * fix: register form title
    
    * fix: mail
    
    * feat: add og image
    
    * feat: add web
    
    * fix: wrong padding jpc
    
    * feat: ping animation
    
    * Update formData.ts
    
    * feat: form validator
    
    * feat: form
    
    * fix:(registerPage): code image test
    
    * fix(multiplechoice): value
    
    * fix: wording and question
    
    * Update page.tsx
    
    * add : carousel container
    
    * fix: logic
    
    * fix: animation
    
    * fix: temporary disable carousel
    
    * fix: animation
    
    * fix: temporary disable carousel
    
    * fix: email validate and step persistent
    
    * fix: add logs
    
    * fix: discordbot log
    
    * fix: separate url
    
    * fix: text
    
    * fix: text space
    
    * feat: carousel
    
    * fix: text label
    
    * feat: error page
    
    * feat: input phone masked
    
    * fix: carousel images
    
    * fix: loading state
    
    * fix z-index
    
    * fix: form
    
    * fix: sendemail
    
    * fix: date
    
    * fix: clean up
    
    * feat: sentry
    
    ---------
    
    Co-authored-by: Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
    Co-authored-by: Jirasin <jjrsinx@gmail.com>
    Co-authored-by: Jirasin Jarethammajit <69670650+jirasin02@users.noreply.github.com>

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bf2523e81ef4226a64b515f79daa2496@o4504563103432704.ingest.sentry.io/4506462437376000",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
