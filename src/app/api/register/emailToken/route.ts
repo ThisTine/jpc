6bc840340a34d8014ef1f465f92234ccb63e620a
sittichok Ouamsiri <sutichok46@gmail.com>
2023-12-27 21:42:12 +0700

    fix: allow encrypt email only in dev

--- src/app/api/register/emailToken/route.ts
+++ src/app/api/register/emailToken/route.ts
@@ -2,7 +2,9 @@ import {NextRequest} from "next/server";
 import {encryptData} from "@/utils/AESEncryption";
 
 export async function GET(request:NextRequest){
-
+  if (process.env.ENV !== "DEV"){
+    return new Response("not allow", {status: 403});
+  }
   const email = request.nextUrl.searchParams.get("email");
   if(!email){
     return new Response("no email", {status: 400});
