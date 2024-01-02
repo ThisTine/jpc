e6f84746884db961be8b22bd2ff6371985a22535
sittichok Ouamsiri <sutichok46@gmail.com>
2024-01-02 20:47:41 +0700

    Phase2 api (#28)
    * chore: add class utility.
    
    * chore: settings
    
    * feat: misc classname
    
    * add: title and info
    
    * add: cs sit info
    
    * update eslint
    
    * add: faq and contact us
    
    * chore: add class utility.
    
    * chore: settings
    
    * feat: misc classname
    
    * add: title and info
    
    * add: cs sit info
    
    * update eslint
    
    * add: faq and contact us
    
    * add: cursor pointer
    
    * add: tablet design
    
    * add: agenda
    
    * fix: horizontal overflow
    
    * fix: horizontal overflow
    
    * fix: migrate font
    
    * fix: ibm font to agenda page
    
    * feat: framer motion
    
    * feat: mobile overflow
    
    * feat: add content
    
    * add: react-hook-form
    
    * feat: add zod, resolver
    
    * feat: animation
    
    * feat: prepared form
    
    * feat: stepper
    
    * add: ig, facebook link
    
    * fix font and title
    
    * feat: react-imask
    
    * fix: localStorage
    
    * fix: alpine
    
    * fix: migrate formData
    
    * feat: formData API
    
    * add css for register
    
    * fix: missing font import
    
    * update faq question
    
    * feat: register mail
    
    * add: link to register
    
    * fix: mobile title
    
    * fix: stepper width and maxWidth container
    
    * feat: add register button animation
    
    * feat: add bounce animation on jigsaw
    
    * Update page.tsx
    
    * feat: animation wow!
    
    * Update faq-contact.tsx
    
    * Update register test question
    
    * fix faq question
    
    * feat: add animation
    
    * fix: fix pnpm
    
    * fix: fix pnpm
    
    * fix: unoptimized
    
    * fix: register form title
    
    * fix: mail
    
    * feat: add og image
    
    * feat: add web
    
    * fix: wrong padding jpc
    
    * feat: ping animation
    
    * Update formData.ts
    
    * feat: form validator
    
    * feat: form
    
    * fix:(registerPage): code image test
    
    * fix(multiplechoice): value
    
    * fix: wording and question
    
    * Update page.tsx
    
    * add : carousel container
    
    * fix: logic
    
    * fix: animation
    
    * fix: temporary disable carousel
    
    * fix: animation
    
    * fix: temporary disable carousel
    
    * fix: email validate and step persistent
    
    * fix: add logs
    
    * fix: discordbot log
    
    * fix: separate url
    
    * fix: text
    
    * fix: text space
    
    * feat: carousel
    
    * fix: text label
    
    * feat: error page
    
    * feat: input phone masked
    
    * fix: carousel images
    
    * fix: loading state
    
    * fix z-index
    
    * fix: form
    
    * fix: sendemail
    
    * fix: date
    
    * fix: clean up
    
    * feat: sentry
    
    * feat: add email
    
    * fix: allow encrypt email only in dev
    
    * fix: email
    
    * fix: params
    
    * fix: line ids
    
    * fix: teamname
    
    * fix: encrypted time
    
    * fix: sheet range
    
    * fix: remove gameboy
    
    * add: personal info and form
    
    * feat: token validation
    
    * feat: token validation
    
    * fix: validation
    
    * feat: send data
    
    * fix: send data
    
    * fix: success page
    
    * fix: lock file
    
    * fix: fix closed personalinfo Form
    
    ---------
    
    Co-authored-by: Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
    Co-authored-by: Jirasin <jjrsinx@gmail.com>
    Co-authored-by: Jirasin Jarethammajit <69670650+jirasin02@users.noreply.github.com>

--- src/app/api/utils/getFormStatus.ts
+++ src/app/api/utils/getFormStatus.ts
@@ -2,4 +2,4 @@ import dayjs from "dayjs";
 
 export const getIsClosedRegisterFrom = ()=> dayjs().isAfter(dayjs("2024-01-01"));
 
-export const getIsClosedPersonalInfoFrom = ()=> dayjs().isAfter(dayjs("2024-01-03 15:00"));
+export const getIsClosedPersonalInfoFrom = ()=> dayjs().isAfter(dayjs("2024-01-03 19:00"));
